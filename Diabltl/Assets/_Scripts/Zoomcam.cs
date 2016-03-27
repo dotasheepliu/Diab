646d08704f22105b605db06efd55d82d4fc30cfd
Tianyang Liu <dotasheepliu@yahoo.com>
2016-03-27 15:08:59 -0400

    Imported animal friends

--- Diabltl/Assets/_Scripts/Zoomcam.cs
+++ Diabltl/Assets/_Scripts/Zoomcam.cs
@@ -22,19 +22,19 @@ public class Zoomcam : MonoBehaviour {
 	}
 
 	void On_Swipe (Gesture gesture) {
-		transform.Translate (Vector3.left * gesture.deltaPosition.x / Screen.width * transform.localPosition.y);
-		transform.Translate (new Vector3 (0, -1, -1) * gesture.deltaPosition.y / Screen.height * transform.localPosition.y / 1.6f);
+		transform.Translate (Vector3.left * gesture.deltaPosition.x / Screen.width * transform.localPosition.y * 1.2f);
+		transform.Translate (new Vector3 (0, -1, -1) * gesture.deltaPosition.y / Screen.height * transform.localPosition.y / 1.6f * 1.2f);
 	}
 
 	void On_PinchIn (Gesture gesture) {
 		if (transform.localPosition.y < 2.4f) {	
-			transform.Translate (Vector3.back * 0.04f * transform.localPosition.y);
+			transform.Translate (Vector3.back * 0.06f * transform.localPosition.y);
 		}
 	}
 
 	void On_PinchOut (Gesture gesture) {	
 		if (transform.localPosition.y > 0.8f) {	
-			transform.Translate (Vector3.forward * 0.04f * transform.localPosition.y);
+			transform.Translate (Vector3.forward * 0.06f * transform.localPosition.y);
 		}
 	}
 }
