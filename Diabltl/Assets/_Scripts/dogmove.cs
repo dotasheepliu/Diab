b6dbe51049b9fab976b590630524fe42f457a2ae
Tianyang Liu <dotasheepliu@yahoo.com>
2016-03-28 01:09:27 -0400

    Import highlight system. Click dog icon will move camera.

--- Diabltl/Assets/_Scripts/dogmove.cs
+++ Diabltl/Assets/_Scripts/dogmove.cs
@@ -14,7 +14,6 @@ public class dogmove : MonoBehaviour {
 
 	void EasyTouch_On_DoubleTap (Gesture gesture) {
 		if (!doglock) {
-			doglock = true;
 			finalp = gesture.GetCurrentPickedObject ().transform.position;
 			transp = finalp - transform.position;
 			GetComponent<Animation> ().Play ("Walk Dog");
@@ -23,6 +22,7 @@ public class dogmove : MonoBehaviour {
 	}
 
 	IEnumerator slowmove () {
+		doglock = true;
 		transform.LookAt (new Vector3 (finalp.x, transform.position.y, finalp.z));
 		speedx = Mathf.Cos (Mathf.Atan (transp.z / transp.x)) * speed;
 		if (transp.x < 0)
@@ -32,7 +32,7 @@ public class dogmove : MonoBehaviour {
 			speedz = -Mathf.Abs (speedz);
 		else
 			speedz = Mathf.Abs (speedz);
-		Debug.Log (speedz);
+		
 		for (int i = 0; i < Mathf.Abs (transp.x / speedx); i++) {
 			yield return new WaitForSeconds (0.01f);
 			transform.position = new Vector3 (transform.position.x + speedx, transform.position.y + 0.001f, transform.position.z + speedz);
